0793a04f3b59010a53e2468d6e2512425c100b80
Gpanayu <gpanayu@yahoo.co.th>
2018-04-10 19:21:54 +0700

    remove unused lib

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express= require('./config/express');
 var mongoose = require('./config/mongoose');
-var passport = require('./config/passport');
 var config = require('./config/config');
 
 
 var db = mongoose();
 var app = express();
-var passport = passport();
 
 var http = require('http');
 var socketIO = require('socket.io');
